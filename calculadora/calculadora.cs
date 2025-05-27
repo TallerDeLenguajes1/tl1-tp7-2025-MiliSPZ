335b162112812fe65bd71a0d4db2a86bd860dcf2
MiliSPZ <mimisofiap21@gmail.com>
2025-05-27 16:22:17 -0300

    Punto 1 calculadora

---
+++ calculadora/calculadora.cs
@@ -0,0 +1,38 @@
+namespace EspacioCalculadora;
+
+public class Calculadora
+{
+    private double resultado=0;
+    public double Resultado { get => resultado; }
+
+    public void Sumar(double termino)
+    {
+        resultado += termino;
+    }
+
+    public void Restar(double termino)
+    {
+        resultado -= termino;
+    }
+
+    public void Multiplicar(double termino)
+    {
+        resultado *= termino;
+    }
+
+    public void Dividir(double termino)
+    {
+        if (termino != 0)
+        {
+            resultado /= termino;
+        }else
+        {
+            Console.WriteLine("No se puede dividir por 0");
+        }
+    }
+
+    public void Limpiar()
+    {
+        resultado = 0;
+    }
+}
